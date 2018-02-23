fc155216fcc8a6b25948bb75485359ff0cfb0316
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-02-23 13:25:12 -0500

    Added a python script for fixing reversibility inconsisitiencies, if any

---
+++ fix_reversibility.py
@@ -0,0 +1,25 @@
+import csv
+import argparse
+import re
+import cobra
+
+if __name__ == "__main__":
+    description = '''Script fixing reactions reversibility based on the arrow type in reaction field'''
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-f", action="store", dest="filename", required=True, help="The path to the csv file")
+
+    # Argument checking section
+    args = parser.parse_args()
+
+with open(args.filename, 'r') as handle:
+    file = list(csv.reader(handle))
+    with open(args.filename, 'w+') as handle_write:
+        file_write = list(csv.reader(handle_write))
+        for i in range(0, len(file)):
+            line = file[i][2]
+            if re.search(r'>', line) is not None:
+                if re.search(r'<', line) is not None:
+                    file[i][7] = str(1)
+                else:
+                    file[i][7] = str(0)
+            handle_write.write(','.join(file[i]) + "\n")
